858ff38702c892781dc34caa0523ca161ec64c13
peanutboi710 <figaro0928@gmail.com>
2026-05-26 09:51:04 +0900

    set get

--- 0526/01_map.js
+++ 0526/01_map.js
@@ -9,6 +9,20 @@ o["e"] = function () {
 console.log(o);
 
 const map = new Map(); // new - 생성자 -> Map()
+
+o["aa"] = 1234;
+o.bb =12345;
+
+map.set("aa", 1234);
+map.set("bb", 12345);
+
+console.log(map);
+console.log(map.get("aa"));
+
+console.log('"aa" in o', "aa" in o)
+console.log('"cc" in o', "cc" in o)
+
+
 for (const c of map) {
-    
+
 }
